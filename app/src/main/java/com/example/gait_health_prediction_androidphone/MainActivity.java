e235a7949f710862d8f592510e0bc5c316b8ef43
Juyeong-Lee <lge2515@naver.com>
2021-07-09 15:29:34 +0900

    modify package name

---
+++ app/src/main/java/com/example/gait_health_prediction_androidphone/MainActivity.java
@@ -0,0 +1,295 @@
+package com.example.gait_health_prediction_androidphone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.AccountPicker;
+
+import static android.content.ContentValues.TAG;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //Using the Accelometer & Gyroscoper
+    private SensorManager mSensorManager = null;
+
+    //Using the Gyroscope
+    private SensorEventListener mGyroLis;
+    private Sensor mGgyroSensor = null;
+
+    //Roll and Pitch
+    private double pitch;
+    private double roll;
+    private double yaw;
+
+    //timestamp and dt
+    private double timestamp;
+    private double dt;
+
+    // for radian -> dgree
+    private double RAD2DGR = 180 / Math.PI;
+    private static final float NS2S = 1.0f / 1000000000.0f;
+
+    //    권한
+    String[] permission_list = {
+//            Manifest.permission.INTERNET,
+            Manifest.permission.GET_ACCOUNTS
+//            Manifest.permission.READ_PHONE_STATE
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Using the Gyroscope & Accelometer
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        //Using the Accelometer
+        mGgyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mGyroLis = new GyroscopeListener();
+
+        //Touch Listener for Accelometer
+        findViewById(R.id.a_start).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        mSensorManager.registerListener(mGyroLis, mGgyroSensor, SensorManager.SENSOR_DELAY_UI);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        mSensorManager.unregisterListener(mGyroLis);
+                        break;
+                }
+                return false;
+            }
+        });
+//
+
+//        checkPermission();
+
+        // 계정확인
+        // Dialog
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//
+//
+        ActivityCompat.requestPermissions(this, new String[]
+                {Manifest.permission.GET_ACCOUNTS}, 1);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.GET_ACCOUNTS);
+
+        AccountManager am = AccountManager.get(this);
+        Account[] accounts = am.getAccounts();
+        System.out.println(accounts.length);
+        String user_name = "";
+        for (Account ac : accounts) {
+            String acname = ac.name;
+            String actype = ac.type;
+            // Take your time to look at all available accounts
+            System.out.println("Accounts : " + acname + ", " + actype);
+           user_name = acname;
+        }
+        Log.e("Tag", "길이:" + user_name);
+
+
+        // ver 2.
+//        int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 0;
+//        // Here, thisActivity is the current activity
+//        if (ContextCompat.checkSelfPermission(this,
+//                android.Manifest.permission.GET_ACCOUNTS)
+//                != PackageManager.PERMISSION_GRANTED) {
+//
+//            // Should we show an explanation?
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                    android.Manifest.permission.GET_ACCOUNTS)) {
+//
+//                // Show an expanation to the user *asynchronously* -- don't block
+//                // this thread waiting for the user's response! After the user
+//                // sees the explanation, try again to request the permission.
+//
+//            } else {
+//
+//                // No explanation needed, we can request the permission.
+//
+//                ActivityCompat.requestPermissions(this,
+//                        new String[]{android.Manifest.permission.GET_ACCOUNTS},
+//                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+//            }
+//        }
+//        String possibleEmail="";
+//        try{
+//            possibleEmail += "************* Get Registered Gmail Account *************\n\n";
+//            Account[] accounts = AccountManager.get(this).getAccountsByType("com.google");
+//
+//            Log.e("EXCEPTION", "mails: " + accounts.length);
+//
+//            for (Account account : accounts) {
+//
+//                possibleEmail += " --> "+account.name+" : "+account.type+" , \n";
+//                possibleEmail += " \n\n";
+//
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        Log.e("EXCEPTION", "mails: " + possibleEmail);
+
+        // ver3.
+//        Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+//                new String[] {"com.google", "com.google.android.legacyimap"},
+//                false, null, null, null, null);
+//        AccountManager accountManager = AccountManager.get(getApplicationContext());
+//        Account[] accounts = accountManager.getAccountsByType(null);
+//        for (Account account : accounts) {
+//            Log.e(TAG, "account: " + account.name + " : " + account.type);
+//        }
+//        Log.e("EXCEPTION", "mails: " + accounts[0]);
+        // ver 4.
+
+//            Intent intent = AccountManager.newChooseAccountIntent(
+//                    null, null, new String[]{"com.google"}, null, null, null, null);
+//            startActivityForResult(intent, REQUEST_CODE);
+//
+//
+//        @Override
+//        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//            super.onActivityResult(requestCode, resultCode, data);
+//
+//            if (requestCode == REQUEST_CODE) {
+//                if (data != null) {
+//                    Bundle extras = data.getExtras();
+//                    final String accountName = extras.getString(AccountManager.KEY_ACCOUNT_NAME);
+//                    final String accountType = extras.getString(AccountManager.KEY_ACCOUNT_TYPE);
+//                    Log.d(TAG, "Account Name: " + accountName);
+//                    Log.d(TAG, "Account Type: " + accountType);
+//                }
+//            }
+//        }
+
+        // ver 5.
+
+
+    }
+
+    public void checkPermission(){
+        //현재 안드로이드 버전이 6.0미만이면 메서드를 종료한다.
+        //안드로이드6.0 (마시멜로) 이후 버전부터 유저 권한설정 필요
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return;
+
+        for(String permission : permission_list){
+            //권한 허용 여부를 확인한다.
+            int chk = checkCallingOrSelfPermission(permission);
+
+            if(chk == PackageManager.PERMISSION_DENIED){
+                //권한 허용을여부를 확인하는 창을 띄운다
+                requestPermissions(permission_list,0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==0)
+        {
+            for(int i=0; i<grantResults.length; i++)
+            {
+                //허용됬다면
+                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
+                }
+                else {
+                    //권한을 하나라도 허용하지 않는다면 앱 종료
+                    Toast.makeText(getApplicationContext(),"앱권한설정하세요", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.e("LOG", "onPause()");
+        mSensorManager.unregisterListener(mGyroLis);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.e("LOG", "onDestroy()");
+        mSensorManager.unregisterListener(mGyroLis);
+    }
+
+    private class GyroscopeListener implements SensorEventListener {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            /* 각 축의 각속도 성분을 받는다. */
+            double gyroX = event.values[0];
+            double gyroY = event.values[1];
+            double gyroZ = event.values[2];
+
+            /* 각속도를 적분하여 회전각을 추출하기 위해 적분 간격(dt)을 구한다.
+             * dt : 센서가 현재 상태를 감지하는 시간 간격
+             * NS2S : nano second -> second */
+            dt = (event.timestamp - timestamp) * NS2S;
+            timestamp = event.timestamp;
+
+            /* 맨 센서 인식을 활성화 하여 처음 timestamp가 0일때는 dt값이 올바르지 않으므로 넘어간다. */
+            if (dt - timestamp * NS2S != 0) {
+
+                /* 각속도 성분을 적분 -> 회전각(pitch, roll)으로 변환.
+                 * 여기까지의 pitch, roll의 단위는 '라디안'이다.
+                 * SO 아래 로그 출력부분에서 멤버변수 'RAD2DGR'를 곱해주어 degree로 변환해줌.  */
+                pitch = pitch + gyroY * dt;
+                roll = roll + gyroX * dt;
+                yaw = yaw + gyroZ * dt;
+
+                Log.e("LOG", "GYROSCOPE           [X]:" + String.format("%.4f", event.values[0])
+                        + "           [Y]:" + String.format("%.4f", event.values[1])
+                        + "           [Z]:" + String.format("%.4f", event.values[2])
+                        + "           [Pitch]: " + String.format("%.1f", pitch * RAD2DGR)
+                        + "           [Roll]: " + String.format("%.1f", roll * RAD2DGR)
+                        + "           [Yaw]: " + String.format("%.1f", yaw * RAD2DGR)
+                        + "           [dt]: " + String.format("%.4f", dt));
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
+}

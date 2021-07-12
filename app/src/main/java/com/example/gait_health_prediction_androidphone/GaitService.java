13564e3d10e6f0163710b64c395a84aa7c034391
moo-nerim <ansgpfla7@naver.com>
2021-07-12 14:28:25 +0900

    Create Background Service ver.2

--- app/src/main/java/com/example/gait_health_prediction_androidphone/GaitService.java
+++ app/src/main/java/com/example/gait_health_prediction_androidphone/GaitService.java
@@ -18,75 +18,65 @@ import androidx.core.app.NotificationCompat;
 import static android.content.ContentValues.TAG;
 
 public class GaitService extends Service {
-    NotificationManager Notifi_M;
-    MainActivity.ServiceThread thread;
 
     public GaitService() {
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Notifi_M = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        myServiceHandler handler = new myServiceHandler();
-        thread = new MainActivity.ServiceThread(handler);
-        thread.stopForever();
-        return START_STICKY;
+    public void onCreate(){
+        super.onCreate();
+        // 서비스는 한번 실행되면 계속 실행된 상태로 있는다.
+        // 따라서 서비스 특성상 intent를 받아서 처리하기에 적합하지않다.
+        // intent에 대한 처리는 onStartCommand()에서 처리해준다.
+        Log.d(TAG, "onCreate() called");
     }
 
-    //서비스가 종료될 때 할 작업
-    public void onDestroy() {
-        myServiceHandler handler = new myServiceHandler();
-        thread = new MainActivity.ServiceThread(handler);
-        thread.start();
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy() called");
     }
 
-    public void start() {
-        myServiceHandler handler = new myServiceHandler();
-        thread = new MainActivity.ServiceThread(handler);
-        thread.start();
+    @Override
+    public IBinder onBind(Intent intent){
+        throw new UnsupportedOperationException("Not yet Implemented"); //자동으로 작성되는 코드
     }
 
-    public void stop() {
-        myServiceHandler handler = new myServiceHandler();
-        thread = new MainActivity.ServiceThread(handler);
-        thread.stopForever();
-    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        Log.d(TAG, "onStartCommand() called");
 
-    public class myServiceHandler extends Handler {
-        @Override
-        public void handleMessage(android.os.Message msg) {
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            Intent intent = new Intent(getApplicationContext(), GaitService.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            PendingIntent pendingIntent = PendingIntent.getActivity(GaitService.this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-            Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                @SuppressLint("WrongConstant")
-                NotificationChannel notificationChannel = new NotificationChannel("my_notification", "n_channel", NotificationManager.IMPORTANCE_MAX);
-                notificationChannel.setDescription("description");
-                notificationChannel.setName("Channel Name");
-                assert notificationManager != null;
-                notificationManager.createNotificationChannel(notificationChannel);
-            }
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(GaitService.this).setSmallIcon(R.drawable.appicon).setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.appicon)).setContentTitle("Title").setContentText("ContentText").setAutoCancel(true).setSound(soundUri).setContentIntent(pendingIntent).setDefaults(Notification.DEFAULT_ALL).setOnlyAlertOnce(true).setChannelId("my_notification").setColor(Color.parseColor("#ffffff"));
-            assert notificationManager != null;
-            int m = (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
-            Calendar cal = Calendar.getInstance();
-            int hour = cal.get(Calendar.HOUR_OF_DAY);
-            if (hour == 18) {
-                notificationManager.notify(m, notificationBuilder.build());
-                thread.stopForever();
-            } else if (hour == 22) {
-                notificationManager.notify(m, notificationBuilder.build());
-                thread.stopForever();
-            }
+        if (intent == null) {
+            return Service.START_STICKY; //서비스가 종료되어도 자동으로 다시 실행시켜줘!
+        } else {
+            String command = intent.getStringExtra("command");
+            String name = intent.getStringExtra("name");
+
+            //Log.d(TAG, "전달받은 데이터: " + command+ ", " +name);
+
+            try{
+                Thread.sleep(5000); //5초동안 정지
+            } catch(Exception e) {}
+
+            Intent showIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+            /**
+             화면이 띄워진 상황에서 다른 액티비티를 호출하는 것은 문제가없으나,
+             지금은 따로 띄워진 화면이 없는 상태에서 백그라운드에서 실행중인 서비스가 액티비티를 호출하는 상황이다.
+             이러한 경우에는 FLAG_ACTIVITY_NEW_TASK 옵션(플래그)을 사용해줘야 한다.
+             **/
+            showIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            showIntent.putExtra("command", "show");
+            showIntent.putExtra("name", name + " from service.");
+
+            // *** 이제 완성된 인텐트와 startActivity()메소드를 사용하여 MainActivity 액티비티를 호출한다. ***
+            startActivity(showIntent);
         }
+
+        return super.onStartCommand(intent, flags, startId);
     }
-}
 
 }

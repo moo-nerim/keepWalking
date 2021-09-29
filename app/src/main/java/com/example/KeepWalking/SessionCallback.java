57fe36573e1093b42b84c78b6384a96247b5877d
moo-nerim <ansgpfla7@naver.com>
2021-09-29 23:36:41 +0900

    Saving data after logging in to KakaoTalk & modifying MainActivity design

--- app/src/main/java/com/example/KeepWalking/SessionCallback.java
+++ app/src/main/java/com/example/KeepWalking/SessionCallback.java
@@ -1,74 +1,77 @@
-package com.example.keepwalking;
-
-import android.util.Log;
-import android.widget.Toast;
-
+//package com.example.keepwalking;
+//
+//import android.util.Log;
+//import android.widget.Toast;
+//
+////import com.google.firebase.database.DatabaseReference;
+////import com.google.firebase.database.FirebaseDatabase;
+//import androidx.annotation.NonNull;
+//
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
 //import com.google.firebase.database.DatabaseReference;
 //import com.google.firebase.database.FirebaseDatabase;
-import androidx.annotation.NonNull;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.kakao.auth.ISessionCallback;
-import com.kakao.network.ErrorResult;
-import com.kakao.usermgmt.UserManagement;
-import com.kakao.usermgmt.callback.MeV2ResponseCallback;
-import com.kakao.usermgmt.response.MeV2Response;
-import com.kakao.usermgmt.response.model.Profile;
-import com.kakao.usermgmt.response.model.UserAccount;
-import com.kakao.util.OptionalBoolean;
-import com.kakao.util.exception.KakaoException;
-
-public class SessionCallback implements ISessionCallback {
-
-    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference databaseReference = firebaseDatabase.getReference();
-
-    // 로그인에 성공한 상태
-    @Override
-    public void onSessionOpened() {
-        requestMe();
-    }
-
-    // 로그인에 실패한 상태
-    @Override
-    public void onSessionOpenFailed(KakaoException exception) {
-        Log.e("SessionCallback :: ", "onSessionOpenFailed : " + exception.getMessage());
-    }
-
-    // 사용자 정보 요청
-    public void requestMe() {
-        UserManagement.getInstance()
-                .me(new MeV2ResponseCallback() {
-                    @Override
-                    public void onSessionClosed(ErrorResult errorResult) {
-                        Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
-                    }
-
-                    @Override
-                    public void onFailure(ErrorResult errorResult) {
-                        Log.e("KAKAO_API", "사용자 정보 요청 실패: " + errorResult);
-                    }
-
-                    @Override
-                    public void onSuccess(MeV2Response result) {
-                        Log.i("KAKAO_API", "사용자 아이디: " + result.getId());
-                        String id = String.valueOf(result.getId());
-                        UserAccount kakaoAccount = result.getKakaoAccount();
-
-                        // Firebase
+//import com.google.firebase.database.ValueEventListener;
+//import com.kakao.auth.ISessionCallback;
+//import com.kakao.network.ErrorResult;
+//import com.kakao.usermgmt.UserManagement;
+//import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+//import com.kakao.usermgmt.response.MeV2Response;
+//import com.kakao.usermgmt.response.model.Profile;
+//import com.kakao.usermgmt.response.model.UserAccount;
+//import com.kakao.util.OptionalBoolean;
+//import com.kakao.util.exception.KakaoException;
+//
+//import java.util.Map;
+//
+//public class SessionCallback implements ISessionCallback {
+//
+//    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+//    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+//
+//    // 로그인에 성공한 상태
+//    @Override
+//    public void onSessionOpened() {
+//        requestMe();
+//    }
+//
+//    // 로그인에 실패한 상태
+//    @Override
+//    public void onSessionOpenFailed(KakaoException exception) {
+//        Log.e("SessionCallback :: ", "onSessionOpenFailed : " + exception.getMessage());
+//    }
+//
+//    // 사용자 정보 요청
+//    public void requestMe() {
+//        UserManagement.getInstance()
+//                .me(new MeV2ResponseCallback() {
+//                    @Override
+//                    public void onSessionClosed(ErrorResult errorResult) {
+//                        Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+//                    }
+//
+//                    @Override
+//                    public void onFailure(ErrorResult errorResult) {
+//                        Log.e("KAKAO_API", "사용자 정보 요청 실패: " + errorResult);
+//                    }
+//
+//                    @Override
+//                    public void onSuccess(MeV2Response result) {
+//                        Log.i("KAKAO_API", "사용자 아이디: " + result.getId());
+//                        String id = String.valueOf(result.getId());
+//                        UserAccount kakaoAccount = result.getKakaoAccount();
+//
+//                        // Firebase
 //                        databaseReference.child("KAKAOID").child(id).addListenerForSingleValueEvent(new ValueEventListener() {
 //                            @Override
 //                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                                String value = snapshot.getValue(String.class);
+//                                Map<String, Object> map = (Map<String, Object>) snapshot.getValue();
 //
-//                                if (value != null) {
+//                                if (map != null) {
 //                                    // Toast.makeText(getApplicationContext(),"이미 존재하는 그룹명입니다.",Toast.LENGTH_SHORT).show();//토스메세지 출력
 //                                } else {
 //                                    //addGroup(Gname_edit.getText().toString(),Gintro_edit.getText().toString(),Gcate_tv.getText().toString(), goaltime, gmp);
+//                                    databaseReference.child("KAKAOID").child(id).push().setValue(id);
 //                                }
 //                            }
 //
@@ -78,45 +81,45 @@ public class SessionCallback implements ISessionCallback {
 //                                //Log.e("MainActivity", String.valueOf(databaseError.toException())); // 에러문 출력
 //                            }
 //                        });
-                        databaseReference.child("KAKAOID").push().setValue(id);
-
-                        if (kakaoAccount != null) {
-
-                            // 이메일
-                            String email = kakaoAccount.getEmail();
-                            Profile profile = kakaoAccount.getProfile();
-                            if (profile == null) {
-                                Log.d("KAKAO_API", "onSuccess:profile null ");
-                            } else {
-                                Log.d("KAKAO_API", "onSuccess:getProfileImageUrl " + profile.getProfileImageUrl());
-                                Log.d("KAKAO_API", "onSuccess:getNickname " + profile.getNickname());
-                            }
-                            if (email != null) {
-
-                                Log.d("KAKAO_API", "onSuccess:email " + email);
-                            }
-
-                            // 프로필
-                            Profile _profile = kakaoAccount.getProfile();
-
-                            if (_profile != null) {
-
-                                Log.d("KAKAO_API", "nickname: " + _profile.getNickname());
-                                Log.d("KAKAO_API", "profile image: " + _profile.getProfileImageUrl());
-                                Log.d("KAKAO_API", "thumbnail image: " + _profile.getThumbnailImageUrl());
-
-                            } else if (kakaoAccount.profileNeedsAgreement() == OptionalBoolean.TRUE) {
-                                // 동의 요청 후 프로필 정보 획득 가능
-
-                            } else {
-                                // 프로필 획득 불가
-                            }
-                        } else {
-                            Log.i("KAKAO_API", "onSuccess: kakaoAccount null");
-                        }
-                    }
-                });
-
-    }
-
-}
+//
+//
+//                        if (kakaoAccount != null) {
+//
+//                            // 이메일
+//                            String email = kakaoAccount.getEmail();
+//                            Profile profile = kakaoAccount.getProfile();
+//                            if (profile == null) {
+//                                Log.d("KAKAO_API", "onSuccess:profile null ");
+//                            } else {
+//                                Log.d("KAKAO_API", "onSuccess:getProfileImageUrl " + profile.getProfileImageUrl());
+//                                Log.d("KAKAO_API", "onSuccess:getNickname " + profile.getNickname());
+//                            }
+//                            if (email != null) {
+//
+//                                Log.d("KAKAO_API", "onSuccess:email " + email);
+//                            }
+//
+//                            // 프로필
+//                            Profile _profile = kakaoAccount.getProfile();
+//
+//                            if (_profile != null) {
+//
+//                                Log.d("KAKAO_API", "nickname: " + _profile.getNickname());
+//                                Log.d("KAKAO_API", "profile image: " + _profile.getProfileImageUrl());
+//                                Log.d("KAKAO_API", "thumbnail image: " + _profile.getThumbnailImageUrl());
+//
+//                            } else if (kakaoAccount.profileNeedsAgreement() == OptionalBoolean.TRUE) {
+//                                // 동의 요청 후 프로필 정보 획득 가능
+//
+//                            } else {
+//                                // 프로필 획득 불가
+//                            }
+//                        } else {
+//                            Log.i("KAKAO_API", "onSuccess: kakaoAccount null");
+//                        }
+//                    }
+//                });
+//
+//    }
+//
+//}

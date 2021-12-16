db29e14480d4c1e1d2bf916bc7afbdd9489d1809
moo-nerim <ansgpfla7@naver.com>
2021-12-16 23:56:25 +0900

    add logout and withdraw functions

--- app/src/main/java/com/example/keepwalking/UserInfo.java
+++ app/src/main/java/com/example/keepwalking/UserInfo.java
@@ -9,8 +9,10 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,7 +21,7 @@ public class UserInfo extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
-    ImageView profile;
+    ImageView profile, logout, withdraw;
     TextView KakaoName;
 
     @Override
@@ -29,6 +31,8 @@ public class UserInfo extends AppCompatActivity {
 
         KakaoName = findViewById(R.id.KakaoName);
         profile = findViewById(R.id.profile);
+        logout = findViewById(R.id.logout);
+        withdraw = findViewById(R.id.withdraw);
 
         // ***** 카카오 프로필 *****
         String url = ((GlobalApplication) getApplication()).getKakaoProfile();
@@ -74,5 +78,21 @@ public class UserInfo extends AppCompatActivity {
             }
         });
         /************* 하단바 *************/
+
+        /*** logout ***/
+        logout.setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+            final Intent intent = new Intent(UserInfo.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        /*** withdraw ***/
+        logout.setOnClickListener(v -> {
+            databaseReference.child("EMAIL").child(((GlobalApplication) getApplication()).getBasicEmail()).removeValue();
+
+            Toast.makeText(getApplicationContext(), "회원탈퇴 되었습니다.", Toast.LENGTH_SHORT).show();
+            final Intent intent = new Intent(UserInfo.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
